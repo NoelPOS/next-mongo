419ae6cd30ad28b7da00e92bc799243393a07d60
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-10 14:22:25 +0700

    fix apibase for env

--- app/supplier/page.js
+++ app/supplier/page.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3000/api/supplier'
+const API_URL = process.env.NEXT_PUBLIC_API_BASE + '/supplier'
 
 function App() {
   const [suppliers, setSuppliers] = useState([])
